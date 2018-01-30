9b73daa6c91b7da5c1e7c8d14275224571bd0904
Huini Du <huinidu@Huinis-MacBook-Air.local>
2018-01-29 20:01:40 -0800

    get NBA logo

---
+++ src/components/TopNavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { TopNavBar } from './TopNavBar'
+import '../styles/App.css'
+
+
+export class TopNavBar extends React.Component {
+
+    render() {
+        return (
+            <div className="App">
+                <TopNavBar/>
+            </div>
+        );
+    }
+}
